aeb384f56dd1a1819cde0b9bf0c7bde4b8d28b81
Najibu Nsubuga <ns.najibu@gmail.com>
2016-06-25 19:47:53 +0100

    TDD for recipe model

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+	def setup
+		@recipe = Recipe.new(name: "Chicken parm", summary: "this is the best chicken parm ever",
+				  description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
+	end
+
+	test "recipe should be valid" do 
+		assert @recipe.valid?
+	end
+
+	test "name should be present" do 
+		@recipe.name = ""
+		assert_not @recipe.valid?
+	end
+end
