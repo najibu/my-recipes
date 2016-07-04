d7ed51be72f4a5fe5f71e039f82e119d4b0966e2
Najibu Nsubuga <ns.najibu@gmail.com>
2016-07-04 20:35:30 +0100

    Added route, sort, count and validation to like feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
 	def index
-		@recipes = Recipe.all
+		@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}
 	end
 
 	def show
@@ -34,6 +34,18 @@ class RecipesController < ApplicationController
 			redirect_to recipe_path(@recipe)
 		else
 			render :edit
+		end
+	end
+
+	def like
+		@recipe = Recipe.find(params[:id])
+		like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+		if like.valid?
+			flash[:success] = "Your selection was successfully"
+			redirect_to :back
+		else
+			flash[:danger] = "You can only like/dislike a recipe once"
+			redirect_to :back
 		end
 	end
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -8,6 +8,16 @@ validates :description, presence: true, length: { minimum: 20, maximum: 500}
 mount_uploader :picture, PictureUploader
 validate :picture_size
 
+def thumbs_up_total
+	self.likes.where(like: true).size
+end
+
+def thumbs_down_total
+	self.likes.where(like: false).size
+end
+	
+
+
 private
 	def picture_size
 		if picture.size > 5.megabytes
