fa8e3328acd976695bb14945e0b0196571e8bb3e
Najibu Nsubuga <ns.najibu@gmail.com>
2016-07-17 21:47:41 +0100

    Added display styles and ingredients

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,4 +1,9 @@
 class StylesController < ApplicationController
+	def show
+		@style = Style.find(params[:id])
+		@recipes = @style.recipes.paginate(page: params[:page], per_page: 4)
+	end
+
 	def new
 		@style = Style.new
 	end
