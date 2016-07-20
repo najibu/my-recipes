bda1209113aa383b250001e1470ae863255767d7
Najibu Nsubuga <ns.najibu@gmail.com>
2016-07-20 15:39:21 +0100

    Added admin user functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -63,7 +63,7 @@ class RecipesController < ApplicationController
 		end
 
 		def require_same_user
-			if current_user != @recipe.chef
+			if current_user != @recipe.chef and !current_user.admin?
 				flash[:danger] = "You can only edit your own recipe"
 				redirect_to recipes_path
 			end
