69b5cf6edb4f4f7db17823b08f411cc3d2ef3778
Najibu Nsubuga <ns.najibu@gmail.com>
2016-07-20 16:21:30 +0100

    Added destroy to admin

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,7 @@ class RecipesController < ApplicationController
 	before_action :require_user, except: [:show, :index, :like]
 	before_action :require_user_like, only: [:like]
 	before_action :require_same_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
 
 	def index
 		@recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -53,6 +54,12 @@ class RecipesController < ApplicationController
 		end
 	end
 
+	def destroy
+		Recipe.find(params[:id]).destroy
+		flash[:success] = "Recipe Deleted"
+		redirect_to recipes_path
+	end
+
 	private
 		def recipe_params
 			params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
@@ -70,9 +77,13 @@ class RecipesController < ApplicationController
 		end
 
 		def require_user_like
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
-      redirect_to :back
-    end
-  end
+		    if !logged_in?
+		      flash[:danger] = "You must be logged in to perform that action"
+		      redirect_to :back
+	    	end
+  		end
+
+  		def admin_user
+			redirect_to recipes_path unless current_user.admin?
+		end
 end
